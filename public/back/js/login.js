d0c5421299a85ccbd9046f7cbe2758ddb28f0165
Your Name <sunshine@example.com>
2018-01-11 19:24:22 +0800

    登录验证完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -4,13 +4,69 @@
 
 $(function () {
 
-  var form =$("form");
+  var $form =$("form");
 
   $form.bootstrapValidator( {
 
-    
+    fields: {
+      username: {
 
-  })
+        validators : {
+          notEmpty:{
+
+            message:"用户名不能为空！"
+          },
+
+          callback:{
+            message:"用户名不存在！"
+          }
+        }
+      },
+
+      password : {
+        validators:{
+          notEmpty:{
+            message:"密码不能为空！"
+          },
+          stringLength:{
+            min:6,
+            max:12,
+            message:"密码必须是6-12位"
+          },
+          callback:{
+            message:"密码错误"
+          }
+        }
+
+      }
+    }
+
+  });
+//需要给表单注册一个校验成功的事件，这个事件在表单校验成功的时候会触发。success.form.bv
+
+  $form.on("success.form.bv",function (e) {
+    //console.log(111);
+    e.preventDefault();
+
+    $.ajax({
+      type:"post",
+      url:'/employee/employeeLogin',
+      data:$form.serialize(),
+      success:function (info) {
+         if(info.success) {
+           location.href ="index.html";
+         }
+
+        if(info.error==1000) {
+          $form.info("bootstrapValidator").updateStatus("username", "INVALID", "callback");
+
+        }
+        if(info.error===1001) {
+          $form.info("bootstrapValidator").updateStatus("password", "INVALID", "callback");
+        }
+      }
+    })
+  });
 
 
 
